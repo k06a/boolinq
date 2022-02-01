5eb90673fad36125d8c7dd49b16f9b897972f3db
Anna “CyberTailor” <cyber@sysrq.in>
2022-02-01 17:55:53 +0500

    Add installation support

--- test/CommonTests.h
+++ test/CommonTests.h
@@ -2,7 +2,7 @@
 
 #include <set>
 
-#include "boolinq.h"
+#include <boolinq/boolinq.h>
 
 using namespace boolinq;
 
--- test/ToStdDequeTest.cpp
+++ test/ToStdDequeTest.cpp
@@ -2,7 +2,7 @@
 
 #include <gtest/gtest.h>
 
-#include "boolinq.h"
+#include <boolinq/boolinq.h>
 
 using namespace boolinq;
 
